a9fc37619567fad6699885845b2ae5da2b05e206
Matias <matias.duro@ina.edu.ar>
2025-08-20 21:27:06 -0300

    clase 2

---
+++ Clase 2/ejercicios/Ejercicio1.js
@@ -0,0 +1,26 @@
+
+// identificar palindromos
+
+
+const palabras = ["Sol", "Ana", "Oso", "Saludo", "Reconocer", "Soldado", "Neuquen"]
+
+function invertirTexto(palabra){
+    // aca se invierte el texto
+    palabra.split()
+    
+}
+
+function esPalindromo(palabras){
+
+    let palabra
+    palabra.toLowerCase()
+
+    // if() 
+
+    // return 
+}
+
+const resultado = esPalindromo(palabras)
+
+console.log("Palabras Palindromo: ", resultado);
+
