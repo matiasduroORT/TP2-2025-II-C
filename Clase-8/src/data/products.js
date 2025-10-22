e955bf631895c8672d86af82615aba3109fd01a5
Matias <matias.duro@ort.edu.ar>
2025-10-22 19:33:37 -0300

    clase 8

---
+++ Clase-8/src/data/products.js
@@ -0,0 +1,14 @@
+export const products = [
+    {
+        id: 1,
+        nombre: 'Batidora',
+        precio: 28,
+        stock: 5
+    }, 
+    {
+        id:2,
+        nombre: 'Licuadora',
+        precio: 35,
+        stock: 5
+    },
+]
